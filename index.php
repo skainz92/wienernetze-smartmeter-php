5c1b2a12ee66b660575bdfa95b39b5c13ddf33c7
Stefan Kainz <83466119+skainz92@users.noreply.github.com>
2022-10-22 18:28:51 +0200

    Remove debugging
    Removed debugging

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set("display_errors", "On");
-
 	require_once("smartmeter-vienna.class.php");
 
 	$sm = new ViennaSmartmeter("[username]", "[password]", false);
